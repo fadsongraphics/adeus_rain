9cc97d35ae55e6d3655f97233b16d39a8dd128ef
Boluwarin001 <boluadediran@gmail.com>
2021-08-13 06:00:57 +0100

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -7,8 +7,10 @@ date_default_timezone_set("Africa/Lagos");
 
 
 // ERRORS
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+error_reporting(0);
+ini_set('display_errors', '0');
 
 
 // DB
